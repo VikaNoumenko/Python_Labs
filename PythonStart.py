d4e65a058d4985e4d520db0371835524d8567f44
VikaNoumenko <vika.noumenko@gmail.com>
2018-02-05 22:45:08 +0400

    first program

---
+++ PythonStart.py
@@ -0,0 +1,59 @@
+print("Hello, Python")
+print(25 / 5)
+print(5 * -5)
+
+print(5 + 5.0)
+
+print(5**2)
+
+print("hello \"PYTHON\"")
+print(input("NAME?"))
+
+p = 1
+print(p)
+
+print("one"+ "two")
+print("Hello" * 5)
+
+test = "String"
+test2 = 2
+print(test + str(test2))
+
+test = True # boolean
+test2 = False
+print(test)
+
+print(1==2)# false
+
+print("dsad" > "fdsf")
+
+p = "D"
+time = "N"
+if p == "D" and time == "N":
+    print("Hello")
+
+#cycles
+
+test = True
+
+while test:
+    print("Hello")
+    test = False
+
+    i = 1
+    while i<= 5:
+        print(i)
+        i = i + 1
+
+#lists
+
+test = [1, 2, 3, 4, 5]
+print(test[2])
+
+test = ["a","b",["d"]]
+print(test[1],[0])
+
+test2 = []
+test.append("Hello")
+test.append(3)
+
